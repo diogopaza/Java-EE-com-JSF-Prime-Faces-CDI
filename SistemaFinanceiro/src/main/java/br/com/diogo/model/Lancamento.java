0275c2b71a25376fc46a70a5aaec854a3686a81c
Diogo Paza <diogopazacvel@gmail.com>
2022-07-15 16:53:38 -0300

    chapter 3.9

--- SistemaFinanceiro/src/main/java/br/com/diogo/model/Lancamento.java
+++ SistemaFinanceiro/src/main/java/br/com/diogo/model/Lancamento.java
@@ -1,9 +1,23 @@
 package br.com.diogo.model;
 
 import java.math.BigDecimal;
-import java.sql.Date;
+import java.util.Date;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="lancamento")
 public class Lancamento {
 	private static final long serialVersionUID = 1L;
 	
@@ -14,42 +28,61 @@ public class Lancamento {
 	private TipoLancamento tipo;
 	private Date dataVencimento;
 	private Date dataPagamento;
+	
+	@Id
+	@GeneratedValue
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public Pessoa getPessoa() {
+	
+	@ManyToOne(optional = false)
+	@JoinColumn(name="pessoa_id")
+	public Pessoa getPessoa() {	
 		return pessoa;
 	}
 	public void setPessoa(Pessoa pessoa) {
 		this.pessoa = pessoa;
 	}
+	
+	@Column(length=80, nullable = false)
 	public String getDescricao() {
 		return descricao;
 	}
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+	
+	@Column(precision = 10, scale =2, nullable = false)
 	public BigDecimal getValor() {
 		return valor;
 	}
 	public void setValor(BigDecimal valor) {
 		this.valor = valor;
 	}
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
 	public TipoLancamento getTipo() {
 		return tipo;
 	}
 	public void setTipo(TipoLancamento tipo) {
 		this.tipo = tipo;
 	}
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "data_vencimento", nullable = false)
 	public Date getDataVencimento() {
 		return dataVencimento;
 	}
 	public void setDataVencimento(Date dataVencimento) {
 		this.dataVencimento = dataVencimento;
 	}
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "data_pagamento")
 	public Date getDataPagamento() {
 		return dataPagamento;
 	}
